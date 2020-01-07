5988c82f5be3a05e65f32191545b4607b2994b22
shankar <52308621+shankar15nov98@users.noreply.github.com>
2020-01-07 12:58:49 -0700

    Add files via upload

---
+++ today.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import {WeatherService} from '../weather.service';
+import { getSyntheticPropertyName } from '@angular/compiler/src/render3/util';
+
+@Component({
+  selector: 'app-today',
+  templateUrl: './today.component.html',
+  styleUrls: ['./today.component.css']
+})
+export class TodayComponent implements OnInit {
+  lat;
+  lon;
+  weather;
+  constructor(private weatherService: WeatherService) { }
+
+  ngOnInit() {
+    this.getLocation();
+  }
+getLocation()
+{
+  if("geolocation" in navigator){
+    navigator.geolocation.watchPosition((success)=>{
+      this.lat=success.coords.latitude;
+      this.lon=success.coords.longitude;
+      this.weatherService.getWeatherDataByCoords(this.lat,this.lon).subscribe(data=>{
+        this.weather=data;
+
+      })
+
+    })
+  }
+  }
+  getCity(city){
+    this.weatherService.getWeatherDataByCityName(city).subscribe((data:any)=>{
+      this.weather=data;
+      this.lat=data.coord.lat;
+      this.lon=data.coord.lon;
+    })
+  }
+}
+
