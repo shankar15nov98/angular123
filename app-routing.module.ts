8bb57fdb7b6b63e64988aa990dec817adf173839
shankar <52308621+shankar15nov98@users.noreply.github.com>
2020-01-07 12:45:51 -0700

    Add files via upload

---
+++ app-routing.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { SearchComponent } from './search/search.component';
+
+
+const routes: Routes = [
+  {
+    path:'',
+    component:HomeComponent,
+    pathMatch:'full'
+  },
+  {
+    path:'home',
+    component:HomeComponent
+  },
+  {
+    path:'search',
+    component:SearchComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
+
+
+
---
+++ app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { HttpClientModule } from '@angular/common/http';
+import { AppComponent } from './app.component';
+import { TodayComponent } from './today/today.component';
+
+import { AgmCoreModule } from '@agm/core';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+   
+    TodayComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+     AgmCoreModule ,
+     apikey:''
+
+   
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
